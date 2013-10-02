83a7d3cf2dd450fac242d2c8a1145f5641f48f9c
Chad Jemmett <chad.jemmett@gmail.com>
2013-10-02 16:28:16 -0600

    added an s to validates_image in the pin model.

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,5 @@
 class Pin < ActiveRecord::Base
+
  attr_accessible :description, :image
  belongs_to :user
 
@@ -6,9 +7,7 @@ class Pin < ActiveRecord::Base
  validates :description, presence: true
 
  has_attached_file :image, styles: { medium: "320x240>"}
- validate_attachment :image, presence: true,
-                                        content_type: {content_type: ["image/jpeg", "image/jpg", "image/png", "image/gif" ] },
-                                        size: {less_than: 5.megabytes}
+ validates_attachment :image, presence: true, content_type: {content_type: ["image/jpeg", "image/jpg", "image/png", "image/gif" ] }, size: {less_than: 5.megabytes}
 
  
 end
