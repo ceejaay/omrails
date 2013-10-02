b60ac3cc8db1c7d0502206af1c7e2698bdbacd39
Chad Jemmett <chad.jemmett@gmail.com>
2013-10-02 15:21:29 -0600

    made minor chages. removed version from paperclip gem in gemfile

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,12 +1,14 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image
+ attr_accessible :description, :image
+ belongs_to :user
 
  validates :user_id, presence: true
  validates :description, presence: true
+
  has_attached_file :image, styles: { medium: "320x240>"}
  validate_attachment :image, presence: true,
                                         content_type: {content_type: ["image/jpeg", "image/jpg", "image/png", "image/gif" ] },
                                         size: {less_than: 5.megabytes}
 
- belongs_to :user
+ 
 end
