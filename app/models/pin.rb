06f1084bf71f80732aa1a3c0d1681ad68a9dbf2a
Chad Jemmett <chad.jemmett@gmail.com>
2013-10-02 10:16:28 -0600

    minor changes to pin model and header partial

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,7 +4,7 @@ class Pin < ActiveRecord::Base
  validates :user_id, presence: true
  validates :description, presence: true
  has_attached_file :image, styles: { medium: "320x240>"}
- validates_attachment :image, presence: true,
+ validate_attachment :image, presence: true,
                                         content_type: {content_type: ["image/jpeg", "image/jpg", "image/png", "image/gif" ] },
                                         size: {less_than: 5.megabytes}
 
